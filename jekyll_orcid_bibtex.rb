7e63ec662d5818ec18efa498084de9d44b4df508
Martin Hinz <martin.hinz@iaw.unibe.ch>
2022-06-21 09:09:40 +0200

    Update jekyll_orcid_bibtex.rb

--- jekyll_orcid_bibtex.rb
+++ jekyll_orcid_bibtex.rb
@@ -12,14 +12,14 @@ module Jekyll
 
       if site.config['author']['orcid']
         url = "https://pub.orcid.org/v2.1/#{orcid}/works"
-        response = JSON.load(open(url, 'Accept' => 'application/json'))
+        response = JSON.load(URI.open(url, 'Accept' => 'application/json'))
       end
 
       put_codes = response['group'].collect{ |entry| entry['work-summary'] }.flatten.collect{ |entry| entry['put-code'] }
 
       bibtex_entries = put_codes.map do |put_code|
         work_url = "https://pub.orcid.org/v2.1/#{orcid}/work/#{put_code}";
-        response = JSON.load(open(work_url, 'Accept' => 'application/json'));
+        response = JSON.load(URI.open(work_url, 'Accept' => 'application/json'));
         response['citation']['citation-value'] if defined? response['citation']['citation-value']
       end .compact!
 
